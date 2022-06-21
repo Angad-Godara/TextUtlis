ae365fa233d85ab81477eeba9c480f24c6385c8b
Angad-Godara <angadrrr2002@gmail.com>
2022-06-21 19:47:14 +0530

    ADD: removing routers for gh hosting

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,22 +1,22 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 
 function Navbar({ mode, changeMode }) {
     return (
         <nav className={`navbar navbar-expand navbar-${mode} bg-${mode}`}>
             <div className="container-fluid">
-                <Link className="navbar-brand" to='/'>TextUtils</Link>
+                <a className="navbar-brand" href='#'>TextUtils</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <Link className="nav-link active" aria-current="page" to='/'>Home</Link>
+                            <a className="nav-link active" aria-current="page" href='#'>Home</a>
                         </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to='/about'>About</Link>
-                        </li>
+                        {/* <li className="nav-item">
+                            <a className="nav-link" href='/'>About</a>
+                        </li> */}
                     </ul>
                     <div className={`form-check form-switch text-${mode === 'light' ? 'dark' : 'light'}`}>
                         <input className="form-check-input" onClick={changeMode} type="checkbox" role="switch" id="flexSwitchCheckDefault" />
@@ -29,3 +29,6 @@ function Navbar({ mode, changeMode }) {
 }
 
 export default Navbar
+
+
+// proptypes are used to set default values of the props and ato check datatypes of the strings
